c3cd0c9e2df5748ea210c56601581df0e1e54300
Bruno <bruno.martins@tcesc.tc.br>
2023-01-24 01:17:07 -0300

    preliminar

---
+++ dados.php
@@ -0,0 +1,35 @@
+<?php
+
+// $nome = "Bruno Ramos Martins";
+// $endereco = "Rua do Bruno numero tal";
+// $telefone = "48 9 9842-8738";
+// $email = "brunoramosmartins@gmail.com";
+
+$clientes = [
+    'Bruno' => [
+        'nome' => 'Bruno Ramos Martins',
+        'endereco' => 'Rua do Bruno numero tal',
+        'telefone' => '48 9 9842-8738',
+        'email' => 'brunoramosmartins@gmail.com'
+    ],
+    'Jessica' => [
+        'nome' => 'Jessica',
+        'endereco' => 'Rua do Bruno numero tal',
+        'telefone' => '48 9 9842-8738',
+        'email' => 'brunoramosmartins@gmail.com'
+    ],
+    'Marcelo' => [
+        'nome' => 'Marcelo',
+        'endereco' => 'Rua do Bruno numero tal',
+        'telefone' => '48 9 9842-8738',
+        'email' => 'brunoramosmartins@gmail.com'
+    ],
+    'Bruna' => [
+        'nome' => 'Bruna',
+        'endereco' => 'Rua do Bruno numero tal',
+        'telefone' => '48 9 9842-8738',
+        'email' => 'brunoramosmartins@gmail.com'
+    ],
+]
+
+?>
--- index.php
+++ index.php
@@ -11,90 +11,23 @@
     <title>Hello, world!</title>
   </head>
   <body>
+    <?php include 'dados.php' ?>
     <div class="container">
-<?php
 
-// Instalando o PHP
-// https://www.youtube.com/watch?v=HzIXZVctwI8
+    <h1>Meu curriculo</h1>
 
-// ########################################
+    <?php foreach($clientes as $v): ?>
 
-// Tipos de variáveis php
+    <p>Nome: <?= $v['nome'] ?></p>
+    <p>Endereço: <?= $v['endereco'] ?></p>
+    <p>Telefone: <?= $v['telefone'] ?></p>
+    <p>email: <?= $v['email'] ?></p>
+    <hr>
 
-// Número
-// $x = 5;
+    <?php endforeach ?>
 
-// String
-// $x = "Bruno Ramos Martins";
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-// Booleano
-// $x = true
-// $x = false
-
-// Nula
-// $x = null;
-
-// Array
-// $x = array(
-//     'nome' => 'bruno',
-//     'idade' => 38,
-//     'sexo' => 'masculino',
-//     'programador' => true
-// );
-
-// Exibir valores de um array
-// print_r($x);
-
-// Exibir array formatado
-// echo "<pre>";
-// print_r($x);
-// echo "</pre>";
-
-// exibindo um item de um array
-// echo $x['nome'];
-
-// Percorrendo as linhas de um array
-// foreach ($x as $k => $v) {
-//     echo $k.": ".$v."<br>";
-// }
-
-// Array bidimensional
-// $x = array(
-//     'cliente1' => array(
-//         'nome' => 'bruno',
-//         'idade' => 38,
-//         'sexo' => 'masculino',
-//         'programador' => true
-//     ),
-//     'cliente2' => array(
-//         'nome' => 'marcelo',
-//         'idade' => 28,
-//         'sexo' => 'masculino',
-//         'programador' => true
-//     )
-// );
-
-// Exibindo um item de um array bidimensional
-// echo $x['cliente2']['nome'];
-
-// Montando uma tabela de um array bidimensional
-// echo "<table class='table'><tr><th>Nome</th><th>Idade</th><th>Sexo</th></tr>";
-// foreach ($x as $k => $v) {
-//     echo "<tr> <td>".$v['nome']."</th><th>".$v['idade']."</th><th>".$v['sexo']."</th> </tr>";
-// }
-// echo "</table>";
-
-
-
-
-?>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-<!-- Option 2: Separate Popper and Bootstrap JS -->
-<!--
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
--->
-</div>    
+    </div>    
 </body>
 </html>
